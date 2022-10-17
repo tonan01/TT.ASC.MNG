341f5fde7b73d202519bf8e96f8cb2d8b2de2fbe
Phạm Gia Bảo <paopaokk321@gmail.com>
2022-10-17 10:20:12 +0700

    lần 3

---
+++ TT.ASC.MNG/Controllers/Lan3Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TT.ASC.MNG.Controllers
+{
+    public class Lan3Controller : Controller
+    {
+        // GET: Lan3
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
