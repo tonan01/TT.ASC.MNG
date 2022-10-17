f931ef42c2a7cf5704104a4f3c56dfc97ff3a2ed
Phạm Gia Bảo <paopaokk321@gmail.com>
2022-10-17 10:19:05 +0700

    testupdalod lầ 2

---
+++ TT.ASC.MNG/Controllers/taomoiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TT.ASC.MNG.Controllers
+{
+    public class taomoiController : Controller
+    {
+        // GET: taomoi
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
